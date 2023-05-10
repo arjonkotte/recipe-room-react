e3851836481d22ea1376b10b0226a821a2166fd6
Arjon Kotte <arjon.kotte@gmail.com>
2023-05-10 19:43:27 -0400

    test 2 heroku deploy

--- server.js
+++ server.js
@@ -14,8 +14,11 @@ app.use(logger('dev'));
 // Content-Type: 'application/json'
 // and put that data on req.body
 app.use(express.json());
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'build')));
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+  app.use(express.static(path.join(__dirname, 'build')));
+}
 
 // middleware that adds the user object from a JWT to req.user
 app.use(require('./config/checkToken'));
